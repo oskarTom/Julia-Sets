6d7ddb7b0ae950f133642197300ad571f37a5a33
Tom Rindell <tomoskar@protonmail.com>
2020-03-20 13:59:16 +0200

    Tehtävä 2 ja 3. Myös korjattu seuraamattomat tiedostot

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,39 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void kortinSaldoAlussaOikein(){
+        assertEquals(10, kortti.saldo());
+    }
+    
+    @Test
+    public void rahanLataaminenKasvattaaSaldoaOikein(){
+        kortti.lataaRahaa(15);
+        assertEquals(25, kortti.saldo());
+    }
+    
+    @Test
+    public void saldoVaheneeOikeinJosRahaaOnTarpeeksi(){
+        kortti.otaRahaa(5);
+        assertEquals(5, kortti.saldo());
+    }
+    
+    @Test
+    public void saldoEiMuutuJosRahaaEiOleTarpeeksi(){
+        kortti.otaRahaa(15);
+        assertEquals(10, kortti.saldo());
+    }
+    
+    @Test
+    public void booleanToimiiOikein(){
+        assertTrue(kortti.otaRahaa(5));
+        assertTrue(kortti.otaRahaa(5));
+        assertTrue(!kortti.otaRahaa(5));
+    }
+    
+    @Test
+    public void paljonkoOnRahaa() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
 }
